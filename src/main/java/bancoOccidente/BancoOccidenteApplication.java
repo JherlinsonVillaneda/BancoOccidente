f33568a37232d6bf6740a36eb0ebd212e044233c
JherlinsonVillaneda <jherlinson10@gmail.com>
2023-09-27 13:37:45 -0500

    Creación del repositorio github.

---
+++ src/main/java/bancoOccidente/BancoOccidenteApplication.java
@@ -0,0 +1,13 @@
+package bancoOccidente;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class  BancoOccidenteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BancoOccidenteApplication.class, args);
+	}
+
+}
